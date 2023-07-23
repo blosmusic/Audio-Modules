b514b65d54e095430dd3c8424669cf305b6cb079
BLOS <blosguitarist@gmail.com>
2023-07-23 13:13:42 +0100

    jc reverb added

--- assets/modules.js
+++ assets/modules.js
@@ -23,6 +23,10 @@ import {
   tremoloSwitch,
 } from "./modules/fxModulesInstances/tremoloFX.js";
 import {
+  vibratoFX,
+  vibratoSwitch,
+} from "./modules/fxModulesInstances/vibratoFX.js";
+import {
   delayFX,
   delaySwitch,
 } from "./modules/fxModulesInstances/delayFX.js";
@@ -34,6 +38,10 @@ import {
   reverbFX,
   reverbSwitch,
 } from "./modules/fxModulesInstances/reverbFX.js";
+import {
+  jcreverbFX,
+  jcreverbSwitch,
+} from "./modules/fxModulesInstances/jcreverbFX.js";
 
 // Create an array to host the FX modules
 const fxModules = [];
@@ -50,11 +58,13 @@ fxModules.push(distortionFX2); // distortion module 2
 fxModules.push(chorusFX); // chorus module
 fxModules.push(phaserFX); // phaser module
 fxModules.push(tremoloFX); // tremolo module
+fxModules.push(vibratoFX); // vibrato module
 
 // Delay and Reverb
 fxModules.push(delayFX); // delay module
 fxModules.push(pingpongDelayFX); // pingpong delay module
 fxModules.push(reverbFX); // reverb module
+fxModules.push(jcreverbFX); // jcreverb module
 
 // add the fx buttons to the fxButtons array
 // Gains, Distortions, Overdrives
@@ -66,10 +76,12 @@ fxButtons.push(distortionSwitch2); // distortion button 2
 fxButtons.push(chorusSwitch); // chorus button
 fxButtons.push(phaserSwitch); // phaser button
 fxButtons.push(tremoloSwitch); // tremolo button
+fxButtons.push(vibratoSwitch); // vibrato button
 
 // Delay and Reverb
 fxButtons.push(delaySwitch); // delay button
 fxButtons.push(pingpongDelaySwitch); // pingpong delay button
 fxButtons.push(reverbSwitch); // reverb button
+fxButtons.push(jcreverbSwitch); // jcreverb button
 
 export { fxModules, fxButtons };
